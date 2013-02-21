af40758c0bd3d1955dfa59d82decbdd1ba7401c5
Daniel Friesel <derf@finalrewind.org>
2013-02-21 23:08:59 +0100

    add munin links, preparations for sunrise and sunset

--- index.pl
+++ index.pl
@@ -17,6 +17,8 @@ sub overview {
 		version => $VERSION,
 		door    => { status => 'closed' },
 		outdoor => { light_door => 1, },
+		sunrise => '12:34:56',
+		sunset  => '21:43:00',
 	);
 	return;
 }
@@ -24,12 +26,25 @@ sub overview {
 helper light_ro => sub {
 	my ($self, $light) = @_;
 
-	my $state = 0;
+	my $state = -1;
 
-	return sprintf('<img src="light_%s.png" class="light ro %s" title="%s" />',
-		$state ? 'on' : 'off',
+	my %imagemap = (
+		'-1' => 'light.png',
+		'0'  => 'light_off.png',
+		'1'  => 'light_on.png',
+	);
+
+	return sprintf('<img src="%s" class="light ro %s" title="%s" />',
+		$imagemap{$state},
 		$light, $light
 	);
+};
+
+helper muninlink => sub {
+	my ($self, $plugin, $name) = @_;
+
+	return sprintf('<a href="https://intern.chaosdorf.de/munin/chaosdorf.dn42/figurehead.chaosdorf.dn42/%s.html">%s</a>',
+		$plugin, $name // $plugin);
 };
 
 helper wikilink => sub {
