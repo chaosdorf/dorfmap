666665de53b5063e7020b8d238743e814bb4cfff
Daniel Friesel <derf@finalrewind.org>
2013-02-27 21:46:30 +0100

    code cleanup

--- index.pl
+++ index.pl
@@ -11,13 +11,13 @@ use Mojolicious::Lite;
 use Storable qw(retrieve);
 
 our $VERSION = '0.00';
-my $locations = {};
+my $locations   = {};
 my $coordinates = {};
 
 sub slurp {
 	my ($file) = @_;
 
-	return read_file($file, err_mode => 'quiet');
+	return read_file( $file, err_mode => 'quiet' );
 }
 
 sub gpio {
@@ -27,29 +27,29 @@ sub gpio {
 }
 
 sub load_coordinates {
-	my @lines = split(/\n/, slurp('coordinates'));
+	my @lines = split( /\n/, slurp('coordinates') );
 
 	for my $line (@lines) {
-		my ($id, $left, $top, $right, $bottom) = split(/\s+/, $line);
+		my ( $id, $left, $top, $right, $bottom ) = split( /\s+/, $line );
 		my $type;
 
-		if (not $id) {
+		if ( not $id ) {
 			next;
 		}
 
-		if ($id =~ s{^([^:]+):}{}o) {
+		if ( $id =~ s{^([^:]+):}{}o ) {
 			$type = $1;
 		}
 
 		# image areas don't specify right and bottom and are usually 32x32px
-		$right //= $left + 32;
+		$right  //= $left + 32;
 		$bottom //= $top + 32;
 
 		$coordinates->{$id} = {
-			x1 => $left,
-			y1 => $top,
-			x2 => $right - $left,
-			y2 => $bottom - $top,
+			x1   => $left,
+			y1   => $top,
+			x2   => $right - $left,
+			y2   => $bottom - $top,
 			type => $type
 		};
 	}
@@ -62,7 +62,7 @@ sub overview {
 
 	$self->render(
 		'overview',
-		version => $VERSION,
+		version     => $VERSION,
 		coordinates => $coordinates,
 	);
 	return;
@@ -72,19 +72,19 @@ sub door_status {
 	my $raw = slurp('/srv/www/door.status');
 	chomp($raw);
 	given ($raw) {
-		when ('open') { return 'open' }
+		when ('open')   { return 'open' }
 		when ('closed') { return 'closed' }
-		default { return 'unknown' }
+		default         { return 'unknown' }
 	}
 }
 
 sub light_ro {
 	my ($light) = @_;
-	my $state = -1;
-	my $image = 'light.png';
+	my $state   = -1;
+	my $image   = 'light.png';
 
 	given ($light) {
-		when ('outdoor') { $state = slurp(gpio(17)) }
+		when ('outdoor') { $state = slurp( gpio(17) ) }
 	}
 
 	given ($state) {
@@ -92,47 +92,46 @@ sub light_ro {
 		when ('0') { $image = 'light_off.png' }
 	}
 
-	return sprintf('<img src="%s" class="light ro %s" title="%s" />',
-		$image,
-		$light, $light
-	);
+	return sprintf( '<img src="%s" class="light ro %s" title="%s" />',
+		$image, $light, $light );
 }
 
 helper has_location => sub {
-	my ($self, $location) = @_;
+	my ( $self, $location ) = @_;
 
-	my $ret = q{};
+	my $ret    = q{};
 	my $prefix = 'http://wiki.chaosdorf.de/Special:URIResolver/';
 
-	for my $item (@{ $locations->{"${prefix}${location}"} // [] } ) {
-		my ($name) = ($item =~ m{ ^ $prefix (.*) $ }x);
+	for my $item ( @{ $locations->{"${prefix}${location}"} // [] } ) {
+		my ($name) = ( $item =~ m{ ^ $prefix (.*) $ }x );
 		$name =~ s{ - ( [0-9A-F] {2} ) }{ chr(hex($1)) }egx;
-		$name = decode('UTF-8', $name);
-		$ret .= sprintf("<li><a href=\"%s\">%s</a></li>\n",
-			$item, $name );
+		$name = decode( 'UTF-8', $name );
+		$ret .= sprintf( "<li><a href=\"%s\">%s</a></li>\n", $item, $name );
 	}
 
 	return $ret;
 };
 
 sub muninlink {
-	my ($plugin, $name) = @_;
+	my ( $plugin, $name ) = @_;
 
-	return sprintf('<a href="https://intern.chaosdorf.de/munin/chaosdorf.dn42/figurehead.chaosdorf.dn42/%s.html">%s</a>',
-		$plugin, $name // $plugin);
+	return
+	  sprintf(
+'<a href="https://intern.chaosdorf.de/munin/chaosdorf.dn42/figurehead.chaosdorf.dn42/%s.html">%s</a>',
+		$plugin, $name // $plugin );
 }
 
 sub server {
-	my ($host, $label) = @_;
+	my ( $host, $label ) = @_;
 	my $image = 'server_off.png';
 	my $state = slurp("/srv/www/${host}.ping");
 
-	if ($state == 1) {
+	if ( $state == 1 ) {
 		$image = 'server_on.png';
 	}
 
-	return sprintf('<img src="%s" class="server %s" title="%s (%s)" />',
-		$image, $host, $host, $label);
+	return sprintf( '<img src="%s" class="server %s" title="%s (%s)" />',
+		$image, $host, $host, $label );
 }
 
 sub sunrise {
@@ -144,36 +143,38 @@ sub sunset {
 }
 
 sub wifi {
-	my ($host, $label) = @_;
+	my ( $host, $label ) = @_;
 	my $image = 'wifi_off.png';
 	my $state = slurp("/srv/www/${host}.ping");
 
-	if ($state == 1) {
+	if ( $state == 1 ) {
 		$image = 'wifi_on.png';
 	}
 
-	return sprintf('<img src="%s" class="wifi ro %s" title="%s" />',
-		$image, $host, $label);
+	return sprintf( '<img src="%s" class="wifi ro %s" title="%s" />',
+		$image, $host, $label );
 }
 
 sub wikilink {
 	my ($site) = @_;
-	my $name = $site;
-	my $image = undef;
+	my $name   = $site;
+	my $image  = undef;
 
-	if ($name =~ s{ ^ Host : }{}ox) {
+	if ( $name =~ s{ ^ Host : }{}ox ) {
 		$image = 'host.png';
 	}
 
-	return sprintf('%s<a href="https://wiki.chaosdorf.de/%s">%s</a>',
+	return sprintf(
+		'%s<a href="https://wiki.chaosdorf.de/%s">%s</a>',
 		$image ? "<img src=\"$image\" />" : q{},
-		$site, $name);
+		$site, $name
+	);
 }
 
 helper statusclass => sub {
-	my ($self, $type, $location) = @_;
+	my ( $self, $type, $location ) = @_;
 
-	if ($type eq 'door') {
+	if ( $type eq 'door' ) {
 		return door_status();
 	}
 
@@ -181,12 +182,12 @@ helper statusclass => sub {
 };
 
 helper statusimage => sub {
-	my ($self, $type, $location) = @_;
+	my ( $self, $type, $location ) = @_;
 
 	given ($type) {
-		when ('light_ro') { return light_ro($location, $location) }
-		when ('server') { return server($location, $location) }
-		when ('wifi') { return wifi($location, $location) }
+		when ('light_ro') { return light_ro( $location, $location ) }
+		when ('server') { return server( $location, $location ) }
+		when ('wifi') { return wifi( $location, $location ) }
 	}
 
 	return q{};
