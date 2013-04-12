5c88029d7e040d4e367b586f62a26f8aa2caa4f1
Daniel Friesel <derf@finalrewind.org>
2013-04-12 20:12:55 +0200

    log preset usecount

--- index.pl
+++ index.pl
@@ -389,7 +389,7 @@ sub sunrisetext {
 
 	my ( $rise_str, $set_str )
 	  = sunrise( $now->year, $now->month, $now->day, 6.47, 51.14,
-		$now->offset / 3600, 0);
+		$now->offset / 3600, 0 );
 
 	my ( $rise_h, $rise_m ) = ( $rise_str =~ m{(..):(..)} );
 	my ( $set_h,  $set_m )  = ( $set_str  =~ m{(..):(..)} );
@@ -848,6 +848,7 @@ get '/presets/apply/:name' => sub {
 
 	if ( exists $presets->{$name}->{timestamp} ) {
 		$presets->{$name}->{timestamp} = time();
+		$presets->{$name}->{usecount}++;
 		save_presets();
 	}
 
