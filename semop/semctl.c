24c0236da17d45fc457712cfd4e7f852cf12bca0
Daniel Friesel <derf@finalrewind.org>
2013-05-11 21:41:19 +0200

    fix semaphore usage

--- semop/semctl.c
+++ semop/semctl.c
@@ -4,9 +4,11 @@ int semid;
 
 void sem_init(key_t key)
 {
-	semid = semget(key, 1, IPC_EXCL|IPC_CREAT|0666);
-	if (semid >= 0)
+	semid = semget(key, 0, 0);
+	if (semid < 0) {
+		semid = semget(key, 1, IPC_EXCL|IPC_CREAT|0666);
 		semctl(semid, 0, SETVAL, (int) 1);
+	}
 }
 
 void sem_enter()
