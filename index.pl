ae2fa0c945a9d2397e067724eb779aa6ab2e01a1
Daniel Friesel <derf@finalrewind.org>
2013-08-27 17:19:51 +0200

    add help title for power usage fields

--- index.pl
+++ index.pl
@@ -356,14 +356,14 @@ sub infotext {
 
 	$buf .= '<span class="wattagetext infohead">power consumption</span>';
 	$buf .= sprintf(
-		'<span class="wattagetext">dorfmap devices</span>'
-		  . '<span class="wattage">ca. %dW</span><br/>',
+'<span class="wattagetext" title="Light, amps, non-serverraum hosts">dorfmap devices</span>'
+		  . '<span class="wattage" title="Serverraum hardware, measured by UPS">ca. %dW</span><br/>',
 		estimated_power_consumption
 	);
-	if (-e "${store_prefix}.power_serverraum") {
+	if ( -e "${store_prefix}.power_serverraum" ) {
 		$buf .= sprintf(
 			'<span class="wattagetext">Serverraum</span>'
-			. '<span class="wattage">%dW</span><br/>',
+			  . '<span class="wattage">%dW</span><br/>',
 			slurp("${store_prefix}.power_serverraum"),
 		);
 	}
