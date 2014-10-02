320e89857f7c47ef36b1873647896ea5cfaf814d
marudor <marudor@marudor.de>
2014-10-02 20:57:21 +0200

    gulpfoo & tooltip yay!

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,6 @@
 var gulp = require('gulp'),
-jshint = require('gulp-jshint'),
-stylish = require('jshint-stylish'),
+prefixer = require('gulp-autoprefixer'),
+csscomb = require('gulp-csscomb'),
 gutil = require('gulp-util'),
 browserify = require('gulp-browserify'),
 concat = require('gulp-concat'),
@@ -9,12 +9,13 @@ jade = require('gulp-jade'),
 exec = require('gulp-exec'),
 plainExec = require('child_process').exec,
 less = require('gulp-less'),
-bower = require('bower'),
 debowerify = require('debowerify'),
 livereload = require('gulp-livereload'),
+beautify = require('gulp-beautify'),
+coffee = require('gulp-coffee'),
+bower = require('bower'),
 fs = require('fs');
 
-var fatalLevel = "off";
 
 //config paths
 var srcBase="src/";
@@ -23,7 +24,7 @@ var pubBase="public/";
 var pubPath = {base:pubBase,css:{pub:pubBase+'css/',bower:srcBase+'css/bower/'},less:srcPath.base+'css',js:pubBase+'js/',jade:pubBase};
 
 gulp.task('perltidy', function() {
-  gulp.src('index.pl')
+  return gulp.src('index.pl')
   .pipe(exec('perl -c <%= file.path %>'))
   .pipe(exec('perltidy -b <%= file.path %>'))
   .pipe(exec('rm <%= file.path %>.bak'))
@@ -32,7 +33,6 @@ gulp.task('perltidy', function() {
 
 gulp.task('perlStart',function() {
   plainExec('MOJO_LISTEN="http://*:8081" MOJO_MODE="development" hypnotoad index.pl');
-  livereload.changed();
 });
 
 gulp.task('perlStop',function() {
@@ -48,26 +48,23 @@ gulp.task('releaseIndicator', function() {
 })
 
 gulp.task('scriptsDebug', ['bower'], function() {
-  fatalLevel = fatalLevel || 'off';
-  gulp.src([srcPath.js])
-  .pipe(jshint())
-  .pipe(jshint.reporter(stylish))
-  .pipe(jshint.reporter('fail'))
+  return gulp.src([srcPath.js], {base: 'src/js/'})
+  .pipe(beautify({indentSize: 2}))
+  .pipe(gulp.dest('src/js/'))
   .pipe(browserify({
     insertGlobals: true,
     transform: ['debowerify'],
     debug: true
   }))
   .pipe(concat('dorfmap.min.js'))
-  .pipe(gulp.dest(pubPath.js));
+  .pipe(gulp.dest(pubPath.js))
+  .on('finish', function(){livereload.changed();});
 });
 
 gulp.task('scriptsRelease', ['bower'], function() {
-  fatalLevel = fatalLevel || 'off';
-  gulp.src([srcPath.js])
-  .pipe(jshint())
-  .pipe(jshint.reporter(stylish))
-  .pipe(jshint.reporter('fail'))
+  return gulp.src([srcPath.js], {base: 'src/js/'})
+  .pipe(beautify({indentSize: 2}))
+  .pipe(gulp.dest('src/js/'))
   .pipe(browserify({
     transform: [[{ global: true, beautify: true, mangle: false }, 'uglifyify'],'debowerify'],
     gzip: true,
@@ -78,29 +75,45 @@ gulp.task('scriptsRelease', ['bower'], function() {
   .pipe(gulp.dest(pubPath.js));
 });
 
-gulp.task('less', ['bower'], function() {
-  gulp.src(srcPath.less)
-  .pipe(less())
-  .pipe(gulp.dest(pubPath.less));
+gulp.task('less', ['bower'], function(cb) {
+  gulp.src(srcPath.less, {base:'src/css/'})
+  .pipe(less({strictMath:true,strictUnit:true}))
+  .pipe(prefixer([
+    'Android >= 4',
+    'Chrome >= 30',
+    'Firefox >= 24', // Firefox 24 is the latest ESR
+    'Explorer >= 9',
+    'iOS >= 6',
+    'Opera >= 24',
+    'Safari >= 6']
+  ))
+  //.pipe(csscomb())
+  .pipe(gulp.dest(pubPath.less))
+  .on('finish',function(){cb()});
 });
 
 gulp.task('css', ['less'], function() {
-  gulp.src([srcPath.css,'bower_components/**/*.css', '!bower_components/kendo-ui/**/*.css'])
+  var depCss = ['angular-material/angular-material.css', 'opentip/css/opentip.css'];
+  depCss=depCss.map(function(c) {return 'bower_components/'+c});
+  depCss.unshift(srcPath.css);
+
+  return gulp.src(depCss)
   .pipe(cssmin({keepSpecialComments:0}))
   .pipe(concat('dorfmap.min.css'))
-  .pipe(gulp.dest(pubPath.css.pub));
+  .pipe(gulp.dest(pubPath.css.pub))
+  .on('finish', function(){livereload.changed();});
 });
 
 
 
 gulp.task('jade', ['bower'], function() {
-  gulp.src([srcPath.jade])
+  return gulp.src([srcPath.jade])
   .pipe(jade())
-  .pipe(gulp.dest(pubPath.jade));
+  .pipe(gulp.dest(pubPath.jade))
+  .on('finish', function(){livereload.changed();});
 });
 
 gulp.task('watch', function() {
-  fatalLevel = fatalLevel || 'off';
   livereload.listen();
   gulp.watch(srcPath.js, ['scriptsDebug', 'perlStart']);
   gulp.watch([srcPath.css], ['css', 'perlStart']);
@@ -118,34 +131,13 @@ gulp.task('bower', function(cb){
   .on('end', function(installed){
     //socket-io-exception
     fs.writeFileSync('bower_components/socket.io-client/socket.io-client.js', fs.readFileSync('bower_components/socket.io-client/socket.io.js'));
-    //kendo-ui images & styles
-    /*
-    var styles = 'bower_components/kendo-ui/styles/';
-    var images = [
-    {dest:pubPath.base+'images/kendo/images/',loc:[styles+'images/',styles+'Silver/']},
-    {dest:pubPath.base+'images/kendo/textures/',loc:[styles+'textures/']}];
-    try {
-    fs.mkdirSync(pubPath.base+'images/kendo');
-  } catch(e){}
-  for (h=0;h<images.length;h++) {
-  try{
-  fs.mkdirSync(images[h].dest);
-} catch(e) {}
-for (i=0;i<images[h].loc.length;i++) {
-var files = fs.readdirSync(images[h].loc[i]);
-for (j=0;j<files.length;j++) {
-fs.writeFileSync(images[h].dest+files[j],fs.readFileSync(images[h].loc[i]+files[j]));
-}
-}
-}
-gulp.src([styles+'*silver.mi*',styles+'*.common.core.*'])
-.pipe(concat('kendo.min.css'))
-.pipe(replace('textures/','kendo/textures/'))
-.pipe(replace('Silver/','images/kendo/images/'))
-.pipe(gulp.dest(pubPath.css.bower));
-*/
-cb();
-});
+    //opentip
+    gulp.src(['bower_components/opentip/src/opentip.coffee','bower_components/opentip/src/adapter-native.coffee'])
+    .pipe(coffee())
+    .pipe(concat('opentip.js'))
+    .pipe(gulp.dest('bower_components/opentip/lib/'))
+    .on('finish', function(){cb()});
+  });
 });
 
 gulp.task('clean',function(){
