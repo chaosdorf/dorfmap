35ac74b331413281eb5ddb18c8c9da7b140ece4e
marudor <marudor@marudor.de>
2015-03-08 12:46:44 +0100

    socketsfoo

--- src/js/Map/Services/socket.js
+++ src/js/Map/Services/socket.js
@@ -1,5 +1,5 @@
 angular.module('Map').factory('socket', ['socketFactory', function (socketFactory) {
   return socketFactory({
-    //ioSocket: io.connect('172.22.26.56:3001')
+    ioSocket: io.connect('172.22.26.56:3001')
   });
 }]);
