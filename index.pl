306c8277c1f127ef7943622318fddd0a0836ed89
Daniel Friesel <derf@finalrewind.org>
2017-02-04 16:15:31 +0100

    perltidy, tmpfoo

--- index.pl
+++ index.pl
@@ -1120,32 +1120,34 @@ post '/action' => sub {
 };
 
 get '/ajax/beamer.json' => sub {
- 	my ($self) = @_;
- 	my $device = $self->param('device');
+	my ($self) = @_;
+	my $device = $self->param('device');
 
- 	$self->render( json => [
- 		{
- 			name => 'PON',
- 			description => 'Power On',
- 		},
- 		{
- 			name => 'POF',
- 			description => 'Power Off',
- 		},
- 		{
- 			name => 'IIS:HD1',
- 			description => 'HDMI',
- 		},
- 		{
- 			name => 'IIS:HD2',
- 			description => 'Chromecast',
- 		},
- 		{
- 			name => 'IIS:RG1',
- 			description => 'VGA',
- 		},
- 	]);
- };
+	$self->render(
+		json => [
+			{
+				name        => 'PON',
+				description => 'Power On',
+			},
+			{
+				name        => 'POF',
+				description => 'Power Off',
+			},
+			{
+				name        => 'IIS:HD1',
+				description => 'HDMI',
+			},
+			{
+				name        => 'IIS:HD2',
+				description => 'Chromecast',
+			},
+			{
+				name        => 'IIS:RG1',
+				description => 'VGA',
+			},
+		]
+	);
+};
 
 # Used by angular frontend. Returns all animations and the
 # currently active one for a given device.
