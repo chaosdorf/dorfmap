8be5dfe21e53bf7763c56894728f977db489d7c0
marudor <marudor@marudor.de>
2018-10-13 00:32:54 +0200

    EventSource

--- src/socket.js
+++ src/socket.js
@@ -1,16 +1,21 @@
 // @flow
 /* eslint import/no-webpack-loader-syntax: 0 */
 import { updateDevice } from 'actions/device';
-import io from 'socket.io-client';
 
-const socket = io.connect(SOCKET_URL || 'http://localhost:3001');
+const url = SOCKET_URL || 'http://localhost:3001';
 
 export function setupSocket(store: *) {
-  socket.on('message', deviceId => {
-    store.dispatch(updateDevice(deviceId));
-  });
+  if (global.EventSource) {
+    const stream = new global.EventSource(`${url}/events`);
+
+    stream.onmessage = e => {
+      if (e.data !== 'PING') {
+        store.dispatch(updateDevice(e.data));
+      }
+    };
+  }
 }
 
 export const socketUpdate = (deviceId: any) => {
-  socket.send(deviceId);
+  fetch(`${url}/fire?id=${deviceId}`);
 };
